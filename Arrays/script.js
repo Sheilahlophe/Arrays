b577d3d05ec086b5db2867a5d059db8e8e8af805
Sheilahlophe <sd.hlophe@gmail.com>
2024-08-05 17:45:33 +0200

    Add files via upload

---
+++ Arrays/script.js
@@ -0,0 +1,6 @@
+// favourite movies
+let favouriteMovies = ['The last air bender', 'Rebel moon', 'Inseption', 'The salt'];
+    console.log(favouriteMovies [0]); // output 'The last air bender'
+    console.log(favouriteMovies [1]); // output 'Rebel moon'
+    console.log(favouriteMovies [2]); // output 'Inseption'
+    console.log(favouriteMovies [3]); // output 'The salt'
